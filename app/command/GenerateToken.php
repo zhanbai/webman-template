1fa6d91a67d0fba5fba53026ab4900b04d6215ed
zhanbai <bai.zhan@qq.com>
2022-09-23 09:52:02 +0800

    command generate token

---
+++ app/command/GenerateToken.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\command;
+
+use app\model\User;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Output\OutputInterface;
+
+
+class GenerateToken extends Command
+{
+    protected static $defaultName = 'wt:generate-token';
+    protected static $defaultDescription = '快速为用户生成 token';
+
+    /**
+     * @return void
+     */
+    protected function configure()
+    {
+        $this->addArgument('id', InputArgument::REQUIRED, '用户 ID');
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $id = $input->getArgument('id');
+
+        $user = User::find($id);
+
+        if (!$user) {
+            $output->writeln('用户不存在');
+            return self::FAILURE;
+        }
+
+        $output->writeln(jwt_encode(['id' => $id]));
+        return self::SUCCESS;
+    }
+
+}
--- app/helpers.php
+++ app/helpers.php
@@ -21,11 +21,11 @@ function envs($key, $default = null)
 // API 成功信息返回封装
 function success($data = null)
 {
-    return json(['code' => 1, 'data' => $data, 'msg' => 'ok']);
+    return json(['code' => 200, 'data' => $data, 'msg' => 'ok']);
 }
 
 // API 失败信息返回封装
-function fail($msg = 'fail', $code = 0)
+function fail($msg = 'fail', $code = 400)
 {
     return json(['code' => $code, 'msg' => $msg]);
 }
